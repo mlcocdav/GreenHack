22ec7980bed067229e98e2de98baf57d82f0a5e9
Konstantin Khokhlov <konstantin.khokhlov@eyen.se>
2023-06-02 19:35:12 +0200

    removed comments

--- app.py
+++ app.py
@@ -54,22 +54,22 @@ class APIs():
         return openai.Embedding.create(input=[text], model=model)['data'][0][
             'embedding']
 
-# def openai_prompt(prompt):
-#     # print(os.getenv("OPENAI_API_KEY"))
-#     openai.api_key = os.environ['OPENAI_API']
-#
-#     response = openai.Completion.create(
-#         model="text-ada-001",
-#         prompt=prompt,
-#         temperature=0,
-#         max_tokens=100,
-#         top_p=1,
-#         frequency_penalty=0.0,
-#         presence_penalty=0.0,
-#         stop=["\n"]
-#     )
-#     return response['choices'][0]['text']
-#
-#
-# iface = gr.Interface(fn=openai_prompt, inputs=["text"], outputs="text")
-# iface.launch()
+def openai_prompt(prompt):
+    # print(os.getenv("OPENAI_API_KEY"))
+    openai.api_key = os.environ['OPENAI_API']
+
+    response = openai.Completion.create(
+        model="text-ada-001",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+    return response['choices'][0]['text']
+
+
+iface = gr.Interface(fn=openai_prompt, inputs=["text"], outputs="text")
+iface.launch()
