ce495a3f7eed372971417a49093c9226c1debc9c
David Mlcoch <mlcoch@spaceknow.com>
2023-06-02 14:12:34 +0200

    Test openai

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import gradio as gr
+import os
+import openai
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 def greet(name):
     return "Hello " + name + "!!"
+
+def openai(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+    return response
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 iface.launch()
